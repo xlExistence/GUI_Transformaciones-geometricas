f0ea7e18fb665cbc7aa25164f319273c604cbddb
Josué Polina <josue.polina19122002@gmail.com>
2024-04-25 11:56:48 -0600

    GUI Tab2D completado
    Fueron implementados y ajustados las entradas de la pantalla 2D en la interfaz del programa

--- GUI_ProyP2.py
+++ GUI_ProyP2.py
@@ -11,7 +11,8 @@ class GUI(ctk.CTk):
 
     # Propiedades de pantalla
         self.title("ProyP2. GUI_Transformaciones geométricas")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{1100}x{600}")
+        self.resizable(False, False)
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -29,14 +30,58 @@ class GUI(ctk.CTk):
         Tab_2D = self.tabview.tab("2D")
 
         # Frame 1
-        frame1 = ctk.CTkFrame(Tab_2D, fg_color="#201C1C")
-        frame1.grid(row=0, column=0, sticky="nsew")
+        self.frame1 = ctk.CTkFrame(Tab_2D, fg_color="#201C1C")
+        self.frame1.grid(row=0, column=0, sticky="nsew")
 
-        label_frame1 = ctk.CTkLabel(frame1, text=f"{frame1.cget("fg_color")}")
-        label_frame1.grid(row=0, column=0)
+        menu_opciones = ctk.CTkOptionMenu(self.frame1, dynamic_resizing=False,
+                                          values=["Rotación", "Traslación", "Espejo", "Cambio de escala"],
+                                          command=self.mostrar_entradas)
+        menu_opciones.place(relx=0.5, rely=0.1, anchor="center")
 
-        # btn = ctk.CTkButton(frame1, command=self.update_window)
-        # btn.grid(row=1, column=0)
+        # Rotación
+        self.rotacion = []
+        self.r_angulo_label = ctk.CTkLabel(self.frame1, text="Ángulo de rotación:", anchor="w")
+        self.rotacion.append(self.r_angulo_label)
+        self.r_angulo_input = ctk.CTkEntry(self.frame1, placeholder_text="Grados (°)")
+        self.rotacion.append(self.r_angulo_input)
+        self.r_coordenadas_centro_label = ctk.CTkLabel(self.frame1, text="Coordenadas del centro de rotación:", anchor="w")
+        self.rotacion.append(self.r_coordenadas_centro_label)
+        self.r_centro_x_input = ctk.CTkEntry(self.frame1, placeholder_text="Eje x")
+        self.rotacion.append(self.r_centro_x_input)
+        self.r_centro_y_input = ctk.CTkEntry(self.frame1, placeholder_text="Eje y")
+        self.rotacion.append(self.r_centro_y_input)
+
+        # Traslación
+        self.traslacion = []
+        self.t_coordenadas_label = ctk.CTkLabel(self.frame1, text="Coordenadas de traslación:", anchor="w")
+        self.traslacion.append(self.t_coordenadas_label)
+        self.t_x_input = ctk.CTkEntry(self.frame1, placeholder_text="Eje x")
+        self.traslacion.append(self.t_x_input)
+        self.t_y_input = ctk.CTkEntry(self.frame1, placeholder_text="Eje y")
+        self.traslacion.append(self.t_y_input)
+
+        # Espejo
+        self.espejo = []
+        self.e_eje_label = ctk.CTkLabel(self.frame1, text="Selecciona el eje de reflexión:", anchor="w")
+        self.espejo.append(self.e_eje_label)
+        self.e_eje = ctk.CTkOptionMenu(self.frame1, dynamic_resizing=False,
+                                          values=["Eje x", "Eje y"])
+        self.espejo.append(self.e_eje)
+        
+        # Cambio de escala
+        self.cambio_escala = []
+        self.ce_coordenadas_centro_label = ctk.CTkLabel(self.frame1, text="Ingresa los nuevos valores de escala:", anchor="w")
+        self.cambio_escala.append(self.ce_coordenadas_centro_label)
+        self.ce_x_input = ctk.CTkEntry(self.frame1, placeholder_text="Factor x")
+        self.cambio_escala.append(self.ce_x_input)
+        self.ce_y_input = ctk.CTkEntry(self.frame1, placeholder_text="Factor y")
+        self.cambio_escala.append(self.ce_y_input)
+
+        # Botón ejecutar
+        btn = ctk.CTkButton(self.frame1, text="Aplicar")
+        btn.place(relx=0.5, rely=0.9, anchor="center")
+
+        self.frame1.grid_rowconfigure(0, weight=0)
 
         # Frame 2
         self.frame2 = ctk.CTkFrame(Tab_2D, fg_color="#818181")
@@ -49,33 +94,20 @@ class GUI(ctk.CTk):
         # canvas.draw()
         # canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-        frame1.grid_rowconfigure(0, weight=1)
-        frame1.grid_rowconfigure(1, weight=5)
-
         Tab_2D.grid_columnconfigure(0, weight=1)
         Tab_2D.grid_columnconfigure(1, weight=2)
         Tab_2D.grid_rowconfigure(0, weight=1)
-
-        # create radiobutton frame
-        # self.radiobutton_frame = customtkinter.CTkFrame(Tab_2D)
-        # self.radiobutton_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
-        # self.radio_var = tkinter.IntVar(value=0)
-        # self.label_radio_group = customtkinter.CTkLabel(master=Tab_2D, text="CTkRadioButton Group:")
-        # self.label_radio_group.grid(row=0, column=1, columnspan=10, padx=10, pady=10, sticky="")
-        # self.radio_button_1 = customtkinter.CTkRadioButton(master=Tab_2D, variable=self.radio_var, value=0)
-        # self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_2 = customtkinter.CTkRadioButton(master=Tab_2D, variable=self.radio_var, value=1)
-        # self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_3 = customtkinter.CTkRadioButton(master=Tab_2D, variable=self.radio_var, value=2)
-        # self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
 
     # Tab 3D
         Tab_3D = self.tabview.tab("3D")
         Tab_3D.grid_columnconfigure(1, weight=1)
 
-        self.update_window()
+        self.mostrar_entradas("Rotación")
+        self.actualizar_plot()
     
-    def update_window(self):
+    # ---------------------------------------
+
+    def actualizar_plot(self):
         fig, _ = plt.subplots()
         canvas = FigureCanvasTkAgg(fig, master=self.frame2)
         canvas.get_tk_widget().pack(side=ctk.TOP, fill=ctk.BOTH, expand=1)
@@ -85,20 +117,41 @@ class GUI(ctk.CTk):
         toolbar.pack(side=ctk.BOTTOM, fill=ctk.X)
 
         self.update()
+
+    def mostrar_entradas(self, opcion):
+        seleccion = ["Rotación", "Traslación", "Espejo", "Cambio de escala"].index(opcion) + 1
+        if seleccion == 1: # Rotación
+            self.r_angulo_label.place(relx=0.1, rely=0.2)
+            self.r_angulo_input.place(relx=0.15, rely=0.26, relwidth=0.7)            
+            self.r_coordenadas_centro_label.place(relx=0.1, rely=0.35)
+            self.r_centro_x_input.place(relx=0.15, rely=0.43, relwidth=0.7)
+            self.r_centro_y_input.place(relx=0.15, rely=0.51, relwidth=0.7)
+
+            self.ocultar_entradas((self.traslacion + self.espejo + self.cambio_escala))
+
+        elif seleccion == 2: # Traslación
+            self.t_coordenadas_label.place(relx=0.1, rely=0.2)
+            self.t_x_input.place(relx=0.15, rely=0.28, relwidth=0.7)
+            self.t_y_input.place(relx=0.15, rely=0.36, relwidth=0.7)
+
+            self.ocultar_entradas((self.rotacion + self.espejo + self.cambio_escala))
+
+        elif seleccion == 3: # Espejo (Reflexión)
+            self.e_eje_label.place(relx=0.1, rely=0.2)
+            self.e_eje.place(relx=0.5, rely=0.3, anchor="center")
+
+            self.ocultar_entradas((self.rotacion + self.traslacion + self.cambio_escala))
         
-    def open_input_dialog_event(self):
-        dialog = ctk.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
+        else: # Cambio de escala
+            self.ce_coordenadas_centro_label.place(relx=0.1, rely=0.2)
+            self.ce_x_input.place(relx=0.15, rely=0.28, relwidth=0.7)
+            self.ce_y_input.place(relx=0.15, rely=0.36, relwidth=0.7)
+            
+            self.ocultar_entradas((self.rotacion + self.traslacion + self.espejo))
 
-    def change_appearance_mode_event(self, new_appearance_mode: str):
-        ctk.set_appearance_mode(new_appearance_mode)
-
-    def change_scaling_event(self, new_scaling: str):
-        new_scaling_float = int(new_scaling.replace("%", "")) / 100
-        ctk.set_widget_scaling(new_scaling_float)
-
-    def sidebar_button_event(self):
-        print("sidebar_button click")
+    def ocultar_entradas(self, e_restantes):
+        for e in e_restantes:
+            e.place_forget()
 
 if __name__ == "__main__":
     app = GUI()
